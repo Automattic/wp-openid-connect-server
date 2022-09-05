43afc52b35e411f1823edd1897421a263adc06c0
Paulo Pinto <paulo.pinto@automattic.com>
2022-09-05 18:04:44 +0100

    Remove comments
    They're accessible through the interfaces.

--- src/OAuth2Storage.php
+++ src/OAuth2Storage.php
@@ -83,32 +83,6 @@ class OAuth2Storage implements ClientInterface,
 		return self::$clients[ $client_id ]['name'];
 	}
 
-	/**
-	 * Fetch authorization code data (probably the most common grant type).
-	 *
-	 * Retrieve the stored data for the given authorization code.
-	 *
-	 * Required for OAuth2::GRANT_TYPE_AUTH_CODE.
-	 *
-	 * @param $code
-	 * Authorization code to be check with.
-	 *
-	 * @return array|null
-	 * An associative array as below, and NULL if the code is invalid
-	 * @code
-	 * return array(
-	 *     "client_id"    => CLIENT_ID,      // REQUIRED Stored client identifier
-	 *     "user_id"      => USER_ID,        // REQUIRED Stored user identifier
-	 *     "expires"      => EXPIRES,        // REQUIRED Stored expiration in unix timestamp
-	 *     "redirect_uri" => REDIRECT_URI,   // REQUIRED Stored redirect URI
-	 *     "scope"        => SCOPE,          // OPTIONAL Stored scope values in space-separated string
-	 * );
-	 * @endcode
-	 *
-	 * @see http://tools.ietf.org/html/rfc6749#section-4.1
-	 *
-	 * @ingroup oauth2_section_4
-	 */
 	public function getAuthorizationCode( $code ) {
 		$term = get_term_by( 'slug', $code, self::TAXONOMY );
 
@@ -132,27 +106,6 @@ class OAuth2Storage implements ClientInterface,
 		return null;
 	}
 
-	/**
-	 * Take the provided authorization code values and store them somewhere.
-	 *
-	 * This function should be the storage counterpart to getAuthCode().
-	 *
-	 * If storage fails for some reason, we're not currently checking for
-	 * any sort of success/failure, so you should bail out of the script
-	 * and provide a descriptive fail message.
-	 *
-	 * Required for OAuth2::GRANT_TYPE_AUTH_CODE.
-	 *
-	 * @param string $code - authorization code to be stored.
-	 * @param mixed  $client_id - client identifier to be stored.
-	 * @param mixed  $user_login - user identifier to be stored.
-	 * @param string $redirect_uri - redirect URI(s) to be stored in a space-separated string.
-	 * @param int    $expires - expiration to be stored as a Unix timestamp.
-	 * @param string $scope - OPTIONAL scopes to be stored in space-separated string.
-	 * @param string $id_token - OPTIONAL the OpenID Connect id_token.
-	 *
-	 * @ingroup oauth2_section_4
-	 */
 	public function setAuthorizationCode( $code, $client_id, $user_login, $redirect_uri, $expires, $scope = null, $id_token = null ) {
 		if ( $code ) {
 			$this->expireAuthorizationCode( $code );
@@ -178,17 +131,6 @@ class OAuth2Storage implements ClientInterface,
 		}
 	}
 
-	/**
-	 * Once an Authorization Code is used, it must be expired
-	 *
-	 * @see http://tools.ietf.org/html/rfc6749#section-4.1.2
-	 *
-	 *    The client MUST NOT use the authorization code
-	 *    more than once.  If an authorization code is used more than
-	 *    once, the authorization server MUST deny the request and SHOULD
-	 *    revoke (when possible) all tokens previously issued based on
-	 *    that authorization code
-	 */
 	public function expireAuthorizationCode( $code ) {
 		$term = get_term_by( 'slug', $code, self::TAXONOMY );
 
@@ -197,31 +139,6 @@ class OAuth2Storage implements ClientInterface,
 		}
 	}
 
-	/**
-	 * Get client details corresponding client_id.
-	 *
-	 * OAuth says we should store request URIs for each registered client.
-	 * Implement this function to grab the stored URI for a given client id.
-	 *
-	 * @param $client_id
-	 * Client identifier to be check with.
-	 *
-	 * @return array
-	 *               Client details. The only mandatory key in the array is "redirect_uri".
-	 *               This function MUST return FALSE if the given client does not exist or is
-	 *               invalid. "redirect_uri" can be space-delimited to allow for multiple valid uris.
-	 *               <code>
-	 *               return array(
-	 *               "redirect_uri" => REDIRECT_URI,      // REQUIRED redirect_uri registered for the client
-	 *               "client_id"    => CLIENT_ID,         // OPTIONAL the client id
-	 *               "grant_types"  => GRANT_TYPES,       // OPTIONAL an array of restricted grant types
-	 *               "user_id"      => USER_ID,           // OPTIONAL the user identifier associated with this client
-	 *               "scope"        => SCOPE,             // OPTIONAL the scopes allowed for this client
-	 *               );
-	 *               </code>
-	 *
-	 * @ingroup oauth2_section_4
-	 */
 	public function getClientDetails( $client_id ) {
 		if ( isset( self::$clients[ $client_id ] ) ) {
 			return array(
@@ -234,12 +151,6 @@ class OAuth2Storage implements ClientInterface,
 		return false;
 	}
 
-	/**
-	 * Get the scope associated with this client
-	 *
-	 * @return string
-	 * STRING the space-delineated scope list for the specified client_id
-	 */
 	public function getClientScope( $client_id ) {
 		if ( isset( self::$clients[ $client_id ]['scope'] ) ) {
 			return self::$clients[ $client_id ]['scope'];
@@ -248,23 +159,6 @@ class OAuth2Storage implements ClientInterface,
 		return '';
 	}
 
-	/**
-	 * Check restricted grant types of corresponding client identifier.
-	 *
-	 * If you want to restrict clients to certain grant types, override this
-	 * function.
-	 *
-	 * @param $client_id
-	 * Client identifier to be check with.
-	 * @param $grant_type
-	 * Grant type to be check with
-	 *
-	 * @return bool
-	 * TRUE if the grant type is supported by this client identifier, and
-	 * FALSE if it isn't.
-	 *
-	 * @ingroup oauth2_section_4
-	 */
 	public function checkRestrictedGrantType( $client_id, $grant_type ) {
 		if ( isset( self::$clients[ $client_id ]['grant_types'] ) ) {
 			return in_array( $grant_type, self::$clients[ $client_id ]['grant_types'], true );
@@ -273,21 +167,6 @@ class OAuth2Storage implements ClientInterface,
 		return false;
 	}
 
-
-	/**
-	 * Return claims about the provided user id.
-	 *
-	 * Groups of claims are returned based on the requested scopes. No group
-	 * is required, and no claim is required.
-	 *
-	 * @param mixed  $user_login - The id of the user for which claims should be returned.
-	 * @param string $scope - The requested scope.
-	 * Scopes with matching claims: profile, email, address, phone.
-	 *
-	 * @return array - An array in the claim => value format.
-	 *
-	 * @see http://openid.net/specs/openid-connect-core-1_0.html#ScopeClaims
-	 */
 	public function getUserClaims( $user_login, $scope ) {
 		$claims = array(
 			// We expose the scope here so that it's in the token (unclear from the specs but the userinfo endpoint reads the scope from the token).
@@ -321,23 +200,6 @@ class OAuth2Storage implements ClientInterface,
 		return $claims;
 	}
 
-
-	/**
-	 * Make sure that the client credentials is valid.
-	 *
-	 * @param $client_id
-	 * Client identifier to be check with.
-	 * @param $client_secret
-	 * (optional) If a secret is required, check that they've given the right one.
-	 *
-	 * @return bool
-	 * TRUE if the client credentials are valid, and MUST return FALSE if it isn't.
-	 * @endcode
-	 *
-	 * @see http://tools.ietf.org/html/rfc6749#section-3.1
-	 *
-	 * @ingroup oauth2_section_3
-	 */
 	public function checkClientCredentials( $client_id, $client_secret = null ) {
 		if ( isset( self::$clients[ $client_id ] ) ) {
 			if ( ! isset( self::$clients[ $client_id ]['secret'] ) ) {
@@ -350,22 +212,6 @@ class OAuth2Storage implements ClientInterface,
 		return false;
 	}
 
-	/**
-	 * Determine if the client is a "public" client, and therefore
-	 * does not require passing credentials for certain grant types
-	 *
-	 * @param $client_id
-	 * Client identifier to be check with.
-	 *
-	 * @return bool
-	 * TRUE if the client is public, and FALSE if it isn't.
-	 * @endcode
-	 *
-	 * @see http://tools.ietf.org/html/rfc6749#section-2.3
-	 * @see https://github.com/bshaffer/oauth2-server-php/issues/257
-	 *
-	 * @ingroup oauth2_section_2
-	 */
 	public function isPublicClient( $client_id ) {
 		return isset( self::$clients[ $client_id ] ) && ! isset( self::$clients[ $client_id ]['secret'] );
 
