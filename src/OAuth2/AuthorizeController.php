0342da9caaaf5422fdfb8c89c8acbea13ab498ea
Paulo Pinto <paulo.pinto@automattic.com>
2022-09-06 10:03:49 +0100

    Fix formatting

--- src/OAuth2/AuthorizeController.php
+++ src/OAuth2/AuthorizeController.php
@@ -1,6 +1,7 @@
 <?php
 
-// phpcs:ignoreFile
+// phpcs:disable WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
+// phpcs:disable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
 
 namespace OpenIDConnectServer\OAuth2;
 
@@ -20,19 +21,20 @@ class AuthorizeController extends BaseAuthorizeController implements AuthorizeCo
 	 * @return array
 	 */
 	protected function buildAuthorizeParameters( $request, $response, $user_id ) {
-		if ( ! $params = parent::buildAuthorizeParameters( $request, $response, $user_id ) ) {
+		$params = parent::buildAuthorizeParameters( $request, $response, $user_id );
+		if ( ! $params ) {
 			return;
 		}
 
 		// Generate an id token if needed.
-		if ( $this->needsIdToken( $this->getScope() ) && $this->getResponseType() == self::RESPONSE_TYPE_AUTHORIZATION_CODE ) {
+		if ( $this->needsIdToken( $this->getScope() ) && $this->getResponseType() === self::RESPONSE_TYPE_AUTHORIZATION_CODE ) {
 			// START MODIFICATION.
 			$userClaims         = $this->clientStorage->getUserClaims( $user_id, $params['scope'] );
 			$params['id_token'] = $this->responseTypes['id_token']->createIdToken( $this->getClientId(), $user_id, $this->getNonce(), $userClaims );
 			// END MODIFICATION.
 		}
 
-		// add the nonce to return with the redirect URI
+		// Add the nonce to return with the redirect URI.
 		$params['nonce'] = $this->getNonce();
 
 		return $params;
