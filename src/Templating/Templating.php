8f27c2818189d317078570a2d65c551b9346c931
Paulo Pinto <paulo.pinto@automattic.com>
2022-09-07 18:39:46 +0100

    Rename variable to data

--- src/Templating/Templating.php
+++ src/Templating/Templating.php
@@ -8,10 +8,10 @@ class Templating {
 		$this->loader = new Loader();
 	}
 
-	public function render( string $template_name, array $parameters ): string {
+	public function render( string $template_name, array $data ): string {
 		ob_start();
 		$this->loader
-			->set_template_data( $parameters, 'parameters' )
+			->set_template_data( $data )
 			->get_template_part( $template_name );
 
 		return ob_get_clean();
