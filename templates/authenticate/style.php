b7fc3c7a0ed7f81746c108d52f20caa9174f02b4
Paulo Pinto <paulo.pinto@automattic.com>
2022-09-08 14:44:34 +0100

    Improve styling of authenticate page

--- templates/authenticate/style.php
+++ templates/authenticate/style.php
@@ -1,7 +1,22 @@
 <style>
-	.openid-connect-authenticate {
+	body {
+		background: #f0f0f1;
+	}
+
+	#openid-connect-authenticate {
 		width: 320px;
 		padding: 5% 0 0;
 		margin: auto;
+	}
+
+	#openid-connect-authenticate #openid-connect-authenticate-form-container {
+		margin-top: 20px;
+		margin-left: 0;
+		padding: 26px 24px 34px;
+		font-weight: 400;
+		overflow: hidden;
+		background: #fff;
+		border: 1px solid #c3c4c7;
+		box-shadow: 0 1px 3px rgba(0, 0, 0, 0.04);
 	}
 </style>
