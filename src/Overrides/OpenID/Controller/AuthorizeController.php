14e364e240011087fe152e25cfc15dcfe2d2bfb2
Paulo Pinto <paulo.pinto@automattic.com>
2022-09-12 14:22:38 +0100

    Typehint id_token response type

--- src/Overrides/OpenID/Controller/AuthorizeController.php
+++ src/Overrides/OpenID/Controller/AuthorizeController.php
@@ -6,6 +6,7 @@
 namespace OpenIDConnectServer\Overrides\OpenID\Controller;
 
 use OAuth2\OpenID\Controller\AuthorizeController as BaseOpenIDAuthorizeController;
+use OAuth2\OpenID\ResponseType\IdToken;
 
 class AuthorizeController extends BaseOpenIDAuthorizeController {
 	protected function buildAuthorizeParameters( $request, $response, $user_id ) {
@@ -16,10 +17,11 @@ class AuthorizeController extends BaseOpenIDAuthorizeController {
 
 		// Generate an id token if needed.
 		if ( $this->needsIdToken( $this->getScope() ) && $this->getResponseType() === self::RESPONSE_TYPE_AUTHORIZATION_CODE ) {
-			// START MODIFICATION.
 			$userClaims         = $this->clientStorage->getUserClaims( $user_id, $params['scope'] );
-			$params['id_token'] = $this->responseTypes['id_token']->createIdToken( $this->getClientId(), $user_id, $this->getNonce(), $userClaims );
-			// END MODIFICATION.
+
+			/** @var IdToken $id_token */
+			$id_token = $this->responseTypes['id_token'];
+			$params['id_token'] = $id_token->createIdToken( $this->getClientId(), $user_id, $this->getNonce(), $userClaims );
 		}
 
 		// Add the nonce to return with the redirect URI.
