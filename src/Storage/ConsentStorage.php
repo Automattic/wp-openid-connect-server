76628fd08c281e9eab25aa74fdf3212091fe3cf3
Ashfame <mail@ashfame.com>
2022-09-29 13:27:51 +0400

    make consent storage client specific

--- src/Storage/ConsentStorage.php
+++ src/Storage/ConsentStorage.php
@@ -3,18 +3,22 @@
 namespace OpenIDConnectServer\Storage;
 
 const STICKY_CONSENT_DURATION = 7 * DAY_IN_SECONDS;
-const META_KEY                = 'oidc_consent_timestamp';
+const META_KEY_PREFIX         = 'oidc_consent_timestamp';
 
 class ConsentStorage {
-	public function needs_consent( $user_id ): bool {
-		$consent_timestamp = absint( get_user_meta( $user_id, META_KEY, true ) );
+	private function get_meta_key( $client_id ) : string {
+		return META_KEY_PREFIX . '_' . $client_id;
+	}
+
+	public function needs_consent( $user_id, $client_id ): bool {
+		$consent_timestamp = absint( get_user_meta( $user_id, $this->get_meta_key( $client_id ), true ) );
 
 		$past_consent_expiry = time() > ( $consent_timestamp + ( STICKY_CONSENT_DURATION ) );
 
 		return empty( $consent_timestamp ) || $past_consent_expiry;
 	}
 
-	public function update_timestamp( $user_id ) {
-		update_user_meta( $user_id, META_KEY, time() );
+	public function update_timestamp( $user_id, $client_id ) {
+		update_user_meta( $user_id, $this->get_meta_key( $client_id ), time() );
 	}
 }
