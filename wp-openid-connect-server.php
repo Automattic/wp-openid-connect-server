01c5bea2f876154f8ddf8fac57e485e16ff17ba2
Ashfame <mail@ashfame.com>
2022-09-29 14:20:55 +0400

    replace need of defining oidc_clients() with a filter as a way of configuring oidc clients

--- wp-openid-connect-server.php
+++ wp-openid-connect-server.php
@@ -23,7 +23,7 @@ add_action(
 			return;
 		}
 
-		$clients = function_exists( '\oidc_clients' ) ? \oidc_clients() : array();
+		$clients = apply_filters( 'oidc_registered_clients', array() ); // Currently the only way to add clients is to use this filter
 		new OpenIDConnectServer( OIDC_PUBLIC_KEY, OIDC_PRIVATE_KEY, $clients );
 	}
 );
