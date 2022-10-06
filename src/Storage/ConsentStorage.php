be3df248f970fd009b38bec5bbbc18e7077932a4
Ashfame <mail@ashfame.com>
2022-10-06 19:01:37 +0400

    change meta_key for consent storage for consistency with auth code data

--- src/Storage/ConsentStorage.php
+++ src/Storage/ConsentStorage.php
@@ -3,11 +3,10 @@
 namespace OpenIDConnectServer\Storage;
 
 const STICKY_CONSENT_DURATION = 7 * DAY_IN_SECONDS;
-const META_KEY_PREFIX         = 'oidc_consent_timestamp';
 
 class ConsentStorage {
 	private function get_meta_key( $client_id ) : string {
-		return META_KEY_PREFIX . '_' . $client_id;
+		return 'oidc_' . $client_id . 'consent_timestamp';
 	}
 
 	public function needs_consent( $user_id, $client_id ): bool {
