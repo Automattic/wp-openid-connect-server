1ebb7f84283d136caa1cf352c13367b3e1a5b4c6
Ashfame <mail@ashfame.com>
2022-10-06 15:10:56 +0400

    make linter happy

--- src/Http/Handlers/AuthenticateHandler.php
+++ src/Http/Handlers/AuthenticateHandler.php
@@ -76,13 +76,12 @@ class AuthenticateHandler extends RequestHandler {
 						?>
 					</h2>
 					<br/>
-					<p><?php echo __( "You don't have permission to use OpenID Connect.", 'wp-openid-connect-server' ) ?></p>
+					<p><?php esc_html_e( "You don't have permission to use OpenID Connect.", 'wp-openid-connect-server' ); ?></p>
 					<br/>
-					<p><?php echo __( "Contact your administrator for more details.", 'wp-openid-connect-server' ) ?></p>
+					<p><?php esc_html_e( 'Contact your administrator for more details.', 'wp-openid-connect-server' ); ?></p>
 					<br/>
 					<p class="submit">
-						<a class="button button-large" href="<?php echo esc_url( $data['cancel_url'] ); ?>"
-						   target="_top">
+						<a class="button button-large" href="<?php echo esc_url( $data['cancel_url'] ); ?>" target="_top">
 							<?php esc_html_e( 'Cancel', 'wp-openid-connect-server' ); ?>
 						</a>
 					</p>
@@ -129,12 +128,10 @@ class AuthenticateHandler extends RequestHandler {
 					<br/>
 					<?php wp_nonce_field( 'wp_rest' ); /* The nonce will give the REST call the userdata. */ ?>
 					<?php foreach ( $data['form_fields'] as $key => $value ) : ?>
-						<input type="hidden" name="<?php echo esc_attr( $key ); ?>"
-							   value="<?php echo esc_attr( $value ); ?>"/>
+						<input type="hidden" name="<?php echo esc_attr( $key ); ?>" value="<?php echo esc_attr( $value ); ?>"/>
 					<?php endforeach; ?>
 					<p class="submit">
-						<input type="submit" name="authorize" class="button button-primary button-large"
-							   value="<?php esc_attr_e( 'Authorize', 'wp-openid-connect-server' ); ?>"/>
+						<input type="submit" name="authorize" class="button button-primary button-large" value="<?php esc_attr_e( 'Authorize', 'wp-openid-connect-server' ); ?>"/>
 						<a href="<?php echo esc_url( $data['cancel_url'] ); ?>" target="_top">
 							<?php esc_html_e( 'Cancel', 'wp-openid-connect-server' ); ?>
 						</a>
