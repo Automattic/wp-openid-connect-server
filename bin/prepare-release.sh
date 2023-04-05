000671396fc430cc5847b0b6db76bb3621945a18
Ashfame <mail@ashfame.com>
2023-04-05 09:12:57 +0400

    stage README for commit since that's also modified

--- bin/prepare-release.sh
+++ bin/prepare-release.sh
@@ -18,7 +18,7 @@ git add composer.json
 sed -i"" -e "s/\(Version: \)\(.*\)/\1          $VERSION/g" openid-connect-server.php
 sed -i"" -e "s/\(Stable tag: \)\(.*\)/\1$VERSION/g" README.md
 rm -f openid-connect-server.php-e README.md-e
-git add openid-connect-server.php
+git add openid-connect-server.php README.md
 
 git commit -m "Release v$VERSION"
 git tag "v$VERSION"
