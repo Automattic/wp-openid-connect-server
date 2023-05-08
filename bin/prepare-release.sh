72c75f5a45ccf5f5840321ce7dcbfae5f6595987
Paulo Pinto <paulo.pinto@automattic.com>
2023-05-08 15:41:08 +0100

    Improve prepare-release script

--- bin/prepare-release.sh
+++ bin/prepare-release.sh
@@ -1,20 +1,42 @@
 set -e
 
+set -e
+
+function error {
+  RED='\033[0;31m'
+  NONE='\033[0m'
+  printf "$RED$1$NONE\n"
+  exit 1
+}
+
 if [ -z "$1" ]; then
-    echo "Provide a new version, current version is $(jq '.version' composer.json)"
-    exit 1
+    error "Provide a new version, current version is $(jq '.version' composer.json)"
 fi
 
 VERSION=$1
-
-# Remove the "v" character (v1.2.3 to 1.2.3)
 if [[ $VERSION == v* ]]; then
-    VERSION="${VERSION#v}"
+  # Strip leading v.
+  VERSION="${VERSION:1}"
 fi
 
-git checkout main
+RELEASE_BRANCH="release-$VERSION"
+
+CURRENT_BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+if [[ "$CURRENT_BRANCH" != "main" ]] && [[ "$CURRENT_BRANCH" != "$RELEASE_BRANCH" ]]; then
+  error "You must be on branch main"
+fi
+
+# Make sure we're up-to-date with origin.
 git fetch
-git pull origin main
+git pull --ff-only origin main
+
+# Checkout or create branch for release.
+if [[ $(git branch --list "$RELEASE_BRANCH") ]]
+then
+  git checkout "$RELEASE_BRANCH"
+else
+  git checkout -b "$RELEASE_BRANCH"
+fi
 
 jq ".version = \"$VERSION\"" composer.json > composer.json.tmp
 mv composer.json.tmp composer.json
@@ -25,8 +47,28 @@ sed -i"" -e "s/\(Stable tag: \)\(.*\)/\1$VERSION/g" README.md
 rm -f openid-connect-server.php-e README.md-e
 git add openid-connect-server.php README.md
 
-git commit -m "Release v$VERSION"
-git tag "v$VERSION"
-git push --tags origin main
+# Show diff and ask for confirmation.
+git --no-pager diff --cached
+printf "\n\n"
+read -p "Would you like to commit, push and open a PR for the above diff? [y|n] " yn
+case $yn in
+	yes|y )
+	  echo "Ok, continuing";;
+	* )
+	  error "Exiting without committing."
+esac
 
-echo "Edit the draft release on GitHub and then commit to SVN manually"
+# Commit and push.
+git commit -m "Release $VERSION"
+git push -u origin "$RELEASE_BRANCH"
+
+# Open PR.
+LATEST_VERSION_TAG=$(git describe --tags --match "[0-9]*" --abbrev=0 HEAD)
+PR_BODY=$(cat <<-EOB
+[Commits since $LATEST_VERSION_TAG](https://github.com/Automattic/wp-openid-connect-server/compare/$LATEST_VERSION_TAG...$RELEASE_BRANCH)
+EOB
+)
+gh pr create --draft --base main --label "Prepare Release" --title "Release $VERSION" --body "$PR_BODY" --assignee @me
+
+echo "A Pull Request has been created for Release $VERSION (see URL above)."
+echo "The release will automatically be created once the Pull Request is merged."
