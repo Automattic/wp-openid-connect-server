fd52ae3390e035dedc143714e18e2fafda43e042
Paulo Pinto <paulo.pinto@automattic.com>
2023-09-19 13:28:05 +0100

    Discover issuer

--- integration-tests/index.ts
+++ integration-tests/index.ts
@@ -1,1 +1,21 @@
-console.log("hello");
+import { Issuer } from "openid-client";
+import * as dotenv from "dotenv"
+import * as fs from "fs";
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
+dotenv.config({ path: ".env" });
+if (fs.existsSync(".env.local")) {
+    dotenv.config({ path: ".env.local", override: true });
+}
+
+const issuerUrl = process.env.ISSUER_URL;
+if (!issuerUrl) {
+    console.error("ISSUER_URL environment variable must be set");
+    process.exit(1);
+}
+
+console.log(`Using issuer ${issuerUrl}`);
+
+const issuer = await Issuer.discover(issuerUrl);
+console.log('Discovered issuer %s %O', issuer.issuer, issuer.metadata);
